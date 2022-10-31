4bde4239b4f6e463ee25eb6220924cf6fbbf1223
YaazanH <yazon.hamwi@gmail.com>
2022-10-31 12:26:46 +0200

    add api gateway , login

---
+++ MilitaryRecruitmentInternalMicroServiceV0.1/APIGateway/Program.cs
@@ -0,0 +1,23 @@
+
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Ocelot.Provider.Consul;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true)
+    .AddEnvironmentVariables();
+
+
+builder.Services.AddOcelot(builder.Configuration);
+
+
+var app = builder.Build();
+await app.UseOcelot();
+
+
+app.Run();
