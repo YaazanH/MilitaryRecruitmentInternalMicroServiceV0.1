e2257da72f8a0880072118e7ca942f312b4216a2
Mohammed Kanaan <hamoda77789@gmail.com>
2022-11-24 12:54:31 +0300

    added cash allowance rabbit

--- MilitaryRecruitmentInternalMicroServiceV0.1/CashAllowanceAPI/Data/CashAllowanceContext.cs
+++ MilitaryRecruitmentInternalMicroServiceV0.1/CashAllowanceAPI/Data/CashAllowanceContext.cs
@@ -7,5 +7,13 @@ namespace CashAllowanceAPI.Data
         { }
 
         public DbSet<CashAllowanceAPI.Models.CashAllowance> CashAllowanceDb { get; set; }
+
+        public DbSet<CashAllowanceAPI.Models.RequestStatues> RequestStatuesDB { get; set; }
+        public DbSet<CashAllowanceAPI.Models.RabbitMQResponce> RabbitMQResponceDB { get; set; }
+        public DbSet<CashAllowanceAPI.Models.RabbitMQobj> RabbitMQobjDB { get; set; }
+        public DbSet<CashAllowanceAPI.Models.UserInfo> UserInfoDB { get; set; }
+
+        public DbSet<CashAllowanceAPI.Models.Age> AgeDB { get; set; }
+        public DbSet<CashAllowanceAPI.Models.FinancialyClear> FinancialyClearDB { get; set; }
     }
 }
